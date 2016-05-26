503c25fb47711a22e07b260fa9cbb6d412817007
Tim Henderson <tim.tadh@gmail.com>
2016-05-26 15:04:59 -0400

    refactored the lexer for style
    Signed-off-by: Tim Henderson <tim.tadh@gmail.com>

--- lexer.go
+++ lexer.go
@@ -1,21 +1,32 @@
 package dot
 
 import (
+	"fmt"
 	"strings"
 )
 
 import (
-	"github.com/timtadh/data-structures/errors"
 	lex "github.com/timtadh/lexmachine"
 	"github.com/timtadh/lexmachine/machines"
 )
 
-var Literals []string
-var Keywords []string
-var Tokens []string
-var TokenIds map[string]int
+var Literals []string // The tokens representing literal strings
+var Keywords []string // The keyword tokens
+var Tokens []string // All of the tokens (including literals and keywords)
+var TokenIds map[string]int // A map from the token names to their int ids
+var Lexer *lex.Lexer // The lexer object. Use this to construct a Scanner
 
+// Called a package initialization. Creates the lexer and populates token lists.
 func init() {
+	initTokens()
+	var err error
+	Lexer, err = initLexer()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func initTokens() {
 	Literals = []string{
 		"[",
 		"]",
@@ -48,47 +59,23 @@ func init() {
 	}
 }
 
-func Skip(*lex.Scanner, *machines.Match) (interface{}, error) {
-	return nil, nil
-}
-
-func AsToken(name string) lex.Action {
-	return func(s *lex.Scanner, m *machines.Match) (interface{}, error) {
-		return s.Token(TokenIds[name], string(m.Bytes), m), nil
-	}
-}
-
-func Literal(s *lex.Scanner, m *machines.Match) (interface{}, error) {
-	return s.Token(TokenIds[string(m.Bytes)], string(m.Bytes), m), nil
-}
-
-var Lexer *lex.Lexer
-
-func init() {
-	var err error
-	Lexer, err = lexer()
-	if err != nil {
-		panic(err)
-	}
-}
-
-func lexer() (*lex.Lexer, error) {
+// Creates the lexer object and compiles the NFA.
+func initLexer() (*lex.Lexer, error) {
 	lexer := lex.NewLexer()
 
 	for _, lit := range Literals {
 		r := "\\" + strings.Join(strings.Split(lit, ""), "\\")
-		lexer.Add([]byte(r), Literal)
+		lexer.Add([]byte(r), token(lit))
 	}
-
 	for _, name := range Keywords {
-		lexer.Add([]byte(strings.ToLower(name)), AsToken(name))
+		lexer.Add([]byte(strings.ToLower(name)), token(name))
 	}
 
-	lexer.Add([]byte(`//[^\n]*\n?`), AsToken("COMMENT"))
-	lexer.Add([]byte(`/\*([^*]|\r|\n|(\*+([^*/]|\r|\n)))*\*+/`), AsToken("COMMENT"))
-	lexer.Add([]byte(`([a-z]|[A-Z])([a-z]|[A-Z]|[0-9]|_)*`), AsToken("ID"))
-	lexer.Add([]byte(`"([^\\"]|(\\.))*"`), AsToken("ID"))
-	lexer.Add([]byte("( |\t|\n|\r)+"), Skip)
+	lexer.Add([]byte(`//[^\n]*\n?`), token("COMMENT"))
+	lexer.Add([]byte(`/\*([^*]|\r|\n|(\*+([^*/]|\r|\n)))*\*+/`), token("COMMENT"))
+	lexer.Add([]byte(`([a-z]|[A-Z])([a-z]|[A-Z]|[0-9]|_)*`), token("ID"))
+	lexer.Add([]byte(`"([^\\"]|(\\.))*"`), token("ID"))
+	lexer.Add([]byte("( |\t|\n|\r)+"), skip)
 	lexer.Add([]byte(`\<`),
 		func(scan *lex.Scanner, match *machines.Match) (interface{}, error) {
 			str := make([]byte, 0, 10)
@@ -111,17 +98,29 @@ func lexer() (*lex.Lexer, error) {
 					match.TC = scan.TC
 					scan.TC = tc + 1
 					match.Bytes = str
-					return AsToken("ID")(scan, match)
+					return token("ID")(scan, match)
 				}
 			}
 			return nil,
-				errors.Errorf("unclosed HTML literal starting at %d, (%d, %d)",
+				fmt.Errorf("unclosed HTML literal starting at %d, (%d, %d)",
 					match.TC, match.StartLine, match.StartColumn)
 		},
 	)
+
 	err := lexer.Compile()
 	if err != nil {
 		return nil, err
 	}
 	return lexer, nil
 }
+
+func skip(*lex.Scanner, *machines.Match) (interface{}, error) {
+	return nil, nil
+}
+
+func token(name string) lex.Action {
+	return func(s *lex.Scanner, m *machines.Match) (interface{}, error) {
+		return s.Token(TokenIds[name], string(m.Bytes), m), nil
+	}
+}
+
