55ba83807939bd31a0efcaa83f88c003c93f7a3e
Tim Henderson <tim.tadh@gmail.com>
2016-05-26 14:06:50 -0400

    More tests for the lexer
    Signed-off-by: Tim Henderson <tim.tadh@gmail.com>

--- lexer_test.go
+++ lexer_test.go
@@ -12,7 +12,7 @@ import (
 	lex "github.com/timtadh/lexmachine"
 )
 
-func TestHello(x *testing.T) {
+func TestIntegration(x *testing.T) {
 	t := (*test.T)(x)
 	t.Log("hello")
 	s, err := Lexer.Scanner([]byte(`
@@ -21,10 +21,32 @@ strict // asdfa asfwe
 /*
 	asdf  */ DIGRAPH // asdf`))
 	t.AssertNil(err)
+	expected := []int{
+		TokenIds["DIGRAPH"],
+		TokenIds["ID"],
+		TokenIds["{"],
+		TokenIds["ID"],
+		TokenIds["->"],
+		TokenIds["ID"],
+		TokenIds[";"],
+		TokenIds["}"],
+		TokenIds["ID"],
+		TokenIds["ID"],
+		TokenIds["ID"],
+		TokenIds["COMMENT"],
+		TokenIds["STRICT"],
+		TokenIds["COMMENT"],
+		TokenIds["COMMENT"],
+		TokenIds["ID"],
+		TokenIds["COMMENT"],
+	}
+	i := 0
 	for tok, err, eof := s.Next(); !eof; tok, err, eof = s.Next() {
 		t.AssertNil(err)
 		token := tok.(*lex.Token)
-		t.Log(Tokens[token.Type], token)
+		t.Log(i, Tokens[expected[i]], Tokens[token.Type], token)
+		t.Assert(token.Type == expected[i], "got %v expected %v", Tokens[token.Type], Tokens[expected[i]])
+		i++
 	}
 }
 
@@ -44,6 +66,23 @@ func match(t *test.T, text, tokenName string) {
 	t.Assert(eof, "did not get eof")
 	t.AssertNil(err)
 	t.Assert(tok == nil, "tok should have been nil %v", tok)
+}
+
+func not_match(t *test.T, matches int, text, tokenName string) {
+	btext := []byte(text)
+	s, err := Lexer.Scanner(btext)
+	t.AssertNil(err)
+	for i := 0; i < matches; i++ {
+		tok, err, eof := s.Next()
+		t.Assert(!eof, "got eof")
+		t.AssertNil(err)
+		t.Log(tok)
+	}
+	tok, err, eof := s.Next()
+	t.Assert(!eof, "got eof")
+	t.Assert(tok == nil, "tok should have been nil %v", tok)
+	t.Assert(err != nil, "got nil err when there should have been an error")
+	t.Logf("As expected for '%v' which looks like a %v\n%v", text, tokenName, err)
 }
 
 func TestLiterals(x *testing.T) {
@@ -89,5 +128,25 @@ func TestID2(x *testing.T) {
 func TestID3(x *testing.T) {
 	t := (*test.T)(x)
 	match(t, `<asdfa <><awefw><awef><aw>awef>`, "ID")
+}
+
+func TestNotID1(x *testing.T) {
+	t := (*test.T)(x)
+	not_match(t, 0, "_asdf", "ID")
+}
+
+func TestNotID2(x *testing.T) {
+	t := (*test.T)(x)
+	not_match(t, 1, "as342*df", "ID")
+}
+
+func TestNotID3(x *testing.T) {
+	t := (*test.T)(x)
+	not_match(t, 0, `"asdf\"`, "ID")
+}
+
+func TestNotID4(x *testing.T) {
+	t := (*test.T)(x)
+	not_match(t, 0, `<"asdf<\>"`, "ID")
 }
 
